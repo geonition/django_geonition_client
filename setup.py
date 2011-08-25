375ecb64e0a04560b83decc43837e63a4178d199
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-25 12:05:17 +0300

    setup file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='geonition_client',
+    version='1.0.0',
+    author='Kristoffer Snabb',
+    url='https://github.com/geonition/django_geonition_client',
+    packages=['geonition_client'],
+)
