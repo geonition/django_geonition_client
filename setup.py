ee605ec25e9bdd900302e66f3d5641e922403299
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-10-06 15:49:33 +0300

    setup updated 2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='geonition_client',
-    version='1.0.0',
+    version='1.0.1',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/django_geonition_client',
     packages=find_packages(),
