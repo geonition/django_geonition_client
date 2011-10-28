52d7ef9c2a71f3c7a8fe4d922f3f420dcc7d3ebc
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-10-28 09:49:42 +0300

    setup version changed to 2.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='geonition_client',
-    version='1.0.2',
+    version='2.0.0',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/django_geonition_client',
     packages=find_packages(),
