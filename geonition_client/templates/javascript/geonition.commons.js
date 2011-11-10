fafb19a105ee5de195e4e3f7e9725b7ebb4369cd
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-11-10 09:46:29 +0200

    changes in javascript files so that they work as it should, sorry that I forgot to comit this earlier

--- geonition_client/templates/javascript/geonition.commons.js
+++ geonition_client/templates/javascript/geonition.commons.js
@@ -1,14 +1,2 @@
-/*
-All functions should be gradually added to this
-object so that it can be used side by side with
-other libraries, and also minimize conflict with
-the users own functions and method.
-*/
-var gnt = {};
 
 
-var CSRF_Cookie_Name   = "{{ CSRF_Cookie_Name }}";
-var api_full_url = "{{ method }}{{ host }}";
-
-{% block library_specific_block %}
-{% endblock %}
--- geonition_client/templates/javascript/geonition.jquery.js
+++ geonition_client/templates/javascript/geonition.jquery.js
@@ -1,9 +1,32 @@
 
-{% extends "javascript/geonition.commons.js" %}
+/*
+The base object for geonition javascript function
+all functions should be under gnt.<app name>.<function name>(<params>)
+*/
+var gnt = {};
 
- 
-{% block library_specific_block %}
 
+/*
+Version of applications
+
+Each application has to update this object.
+*/
+gnt.app_version = {};
+gnt.app_version.geonition_client = "2.0.0";
+
+/*
+configure parameters
+
+default values below
+*/
+gnt.config = {};
+gnt.config.CSRF_cookie_name = "{{ CSRF_Cookie_Name }}";
+gnt.config.api_full_url = "{{ method }}{{ host }}";
+
+
+/*
+added to all AJAX calls to the server
+*/
 $(document).ajaxSend(function(event, xhr, settings) {
     function getCookie(name) {
         var cookieValue = null;
@@ -36,37 +59,12 @@ $(document).ajaxSend(function(event, xhr, settings) {
         return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
     }
     if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
-        xhr.setRequestHeader("X-CSRFToken", getCookie(CSRF_Cookie_Name));
+        xhr.setRequestHeader("X-CSRFToken",
+                             getCookie(gnt.config.CSRF_cookie_name));
     }
 });
-
-
-function checkRegexp( o, regexp ) {
-  if ( !( regexp.test( o ) ) ) {
-     return false;
-  }
-  else {
-    return true;
- }
-}
-
-function validate_email(email_address){
-	if (email_address != null && email_address.length > 6){
-	  isEmailValid = checkRegexp( email_address, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-	  if (!isEmailValid){
-	    return false;
-	  }
-	  else {
-	    return true;
-	  }
-	}
-	else{
-	  return false;
-  	}
-}
 
 {% for client in geonition_clients %}
     {{ client }}
 {% endfor %}
 
-{% endblock %}
