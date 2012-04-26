99bbefbcaf30d9661a30b2fe2c510f9a67f69099
Kristoffer Snabb <ksnabb@ubuntu-virtual.(none)>
2012-04-26 12:03:25 +0300

    baseline version 4.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='geonition_client',
-    version='2.0.0',
+    version='4.0.0',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/django_geonition_client',
     packages=find_packages(),
